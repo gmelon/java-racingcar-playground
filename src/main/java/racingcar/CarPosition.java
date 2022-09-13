6c481480361ba9853fa4a9a61007ba63a86828ae
gmelon <hsh1769@naver.com>
2022-09-13 11:06:16 +0900

    feat: CarPosition 클래스

---
+++ src/main/java/racingcar/CarPosition.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public class CarPosition {
+
+    private int position;
+
+    public void stepForward() {
+        position++;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -5,20 +5,42 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
-public class CarNameTest {
+public class CarTest {
 
+    /**
+     * CarName Test
+     */
     @Test
     void carName_기본생성() {
         String carName = "hyun";
         CarName carNameObj = new CarName(carName);
-        assertThat(carNameObj.getCarName()).isEqualTo(carName);
+
+        assertThat(carNameObj.getName()).isEqualTo(carName);
     }
 
     @Test
     void carName은_5자리를_초과할수업다() {
         String carName = "sanghyeok";
+
         assertThatThrownBy(() -> new CarName(carName))
             .isInstanceOf(IllegalArgumentException.class);
     }
+
+    /**
+     * Position Test
+     */
+    @Test
+    void position_생성_후_전진() {
+        CarPosition carPosition = new CarPosition();
+        carPosition.stepForward();
+
+        assertThat(carPosition.getPosition()).isEqualTo(1);
+    }
+
+    /**
+     * Car Tet
+     */
+
+
 
 }
